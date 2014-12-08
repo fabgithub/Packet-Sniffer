c763f8b577a3cc6bc680a517661562abb6e24c27
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2014-12-07 23:46:09 -0800

    Project Structure.

---
+++ src/c/sniffer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * The main entry point of the packet sniffer.
+ *
+ * @param  argc Argument Count
+ * @param  argv Arguments
+ * @return
+ */
+int main(int argc, char **argv)
+{
+
+}
