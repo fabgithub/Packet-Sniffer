94e3250628c5f56261d1083d560692c8c29796ce
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2014-12-08 02:56:22 -0800

    Forget header file.

--- src/c/network.h
+++ src/c/network.h
@@ -5,6 +5,7 @@
 
 void print_network_interfaces();
 int create_raw_socket();
+int create_raw_promiscuous_socket(char *interface);
 struct ifreq* get_network_interface_flags(int sock, char *interface);
 void set_promiscuous_mode(int sock, char *interface);
 void unset_promiscuous_mode(int sock, char *interface);
