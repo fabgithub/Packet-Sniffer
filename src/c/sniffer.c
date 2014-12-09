0c61eab2ad34efc9437ce42aa0ecac47cfaeb885
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2014-12-09 13:17:02 -0800

    Setup + printing network interfaces.

--- src/c/sniffer.c
+++ src/c/sniffer.c
@@ -1,4 +1,5 @@
 #include "includes.h"
+#include "network.h"
 
 /* Interfaces */
 void signal_int();
@@ -17,6 +18,18 @@ int main(int argc, char **argv)
 {
     struct sockaddr saddr;
     int saddr_size, data_size;
+
+    //buffer to scan into
+    unsigned char *buffer = (unsigned char *) malloc(65536);
+
+    //print all available network interfaces
+    printf("%s\n\n", "The available network interfaces on your machine are: ");
+    print_network_interfaces();
+    printf("\n%s\t", "Enter the interface that you want to sniff:");
+    scanf("%s", interface); //assign the global interface
+
+    //get a raw socket in a promiscuous mode
+    socket = create_raw_promiscuous_socket(interface);
 
     //register signal handler for CTRL+C
     signal(SIGINT, signal_int);
