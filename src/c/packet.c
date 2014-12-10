d35b2b14828a693239ce0afa0408f65a8a84ef5f
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2014-12-09 16:57:40 -0800

    Dispatch processing to protocol related methods.

--- src/c/packet.c
+++ src/c/packet.c
@@ -13,4 +13,23 @@ void process_packet(unsigned char* buffer, int buffer_size)
 {
     //process link layer and see what protocol is being used.
     struct ethhdr *eth = (struct ethhdr *) buffer;
+
+    switch(eth->h_proto)
+    {
+        case ETHERTYPE_IP: //IP
+            process_ip_packet(buffer, buffer_size);
+
+            break;
+
+        case ETHERTYPE_ARP: //ARP
+            process_arp_packet(buffer, buffer_size);
+
+            break;
+
+        default: //Other protocols..others in `linux/if_ether.h` and `net/ethernet.h`
+
+            break;
+    }
+
+    //print all stats here.
 }
